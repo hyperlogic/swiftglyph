1e6c33705e622afd61cb2a82cebc4edd101506a4
Anthony Thibault <ajt@hyperlogic.org>
2011-03-04 09:56:45 -0800

    Windows build

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,21 +2,37 @@
 
 require 'compile'
 
-$verbose = false
+# figure out the build platform
+case `uname`.chomp
+when /mingw32.*/i
+  $PLATFORM = :windows
+when "Darwin"
+  $PLATFORM = :darwin
+when "Linux"
+  $PLATFORM = :linux
+else
+  $PLATFORM = :unknown
+end
+
+$verbose = true
 
 CC = 'g++'
 
 # compiler flags
-C_FLAGS = ['-Wall', `freetype-config --cflags`.chomp]
+freetype_config_command = $PLATFORM == :windows ? "sh freetype-config" : "freetype-config"
+
+c_flags = ['-Wall', `#{freetype_config_command} --cflags`.chomp]
 
 # linker flags
-L_FLAGS = [`freetype-config --libs`.chomp]
+l_flags = [`#{freetype_config_command} --libs`.chomp]
+l_flags << "-Lc:/MinGW/lib" if $PLATFORM == :windows
+
 OBJECTS = ['swiftglyph.o', 'tga.o']
 EXE = ['swiftglyph']
 
 Compile.setup(:cc => CC,
-              :c_flags => C_FLAGS,
-              :l_flags => L_FLAGS,
+              :c_flags => c_flags,
+              :l_flags => l_flags,
               :objects => OBJECTS,
               :exe => EXE)
 
