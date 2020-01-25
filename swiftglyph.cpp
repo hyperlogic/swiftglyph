c44a43d582d8435de05a5f6aa1d1412a9391d274
Anthony J. Thibault <tony@highfidelity.io>
2020-01-25 14:15:40 -0800

    remove -flip from convert for raw textures

--- swiftglyph.cpp
+++ swiftglyph.cpp
@@ -393,7 +393,7 @@ int main(int argc, char** argv)
         std::string fn = fontprefix + std::string(".png");
         char cmd[512];
         //sprintf(cmd, "sips -s format png temp.tga --out %s", fn.c_str());
-        sprintf(cmd, "magick convert -flip temp.tga %s", fn.c_str());
+        sprintf(cmd, "magick convert temp.tga %s", fn.c_str());
         system(cmd);
 
         remove("temp.tga");
@@ -415,7 +415,7 @@ int main(int argc, char** argv)
             // scale the image for each mip-level
             // use magick instead of sips (because sips is only available on macos)
             //sprintf(cmd, "sips -s format tga --resampleWidth %d --flip vertical temp.tga --out temp2.tga", w);
-            sprintf(cmd, "magick convert -flip -scale %dx%d temp.tga temp2.tga", w, w);
+            sprintf(cmd, "magick convert -scale %dx%d temp.tga temp2.tga", w, w);
             system(cmd);
 
             // stream into an intensity alpha format
